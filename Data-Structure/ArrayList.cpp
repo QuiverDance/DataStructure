350d0479c21712a01d2d9334dc9990f1a3c9d97f
여름 <pjw7200@ajou.ac.kr>
2021-01-14 21:25:07 +0900

    ADT와 변수명, 함수명을 참고하여 설계한 배열리스트

--- Data-Structure/ArrayList.cpp
+++ Data-Structure/ArrayList.cpp
@@ -1,7 +1,104 @@
+#include "ArrayList.h"
 #include <iostream>
 
+ArrayList::ArrayList(int maxElementCount)
+{
+	if (maxElementCount > 0)
+	{
+		this->maxElementCount = maxElementCount;
+		pElement = new ArrayListNode[maxElementCount];
+		this->currentElementCount = 0;
+	}
+	else
+	{
+		std::cout << "Error: 최대원소개수는 0보다 커야합니다." << std::endl;
+		return;
+	}
+}
 
-int main(void) {
-	std::cout<<"Hello world!";
-	std::cout<<"Hello world!";
+void ArrayList::deleteList()
+{
+	delete pElement;
+}
+
+bool ArrayList::isFull()
+{
+	if (currentElementCount == maxElementCount)
+		return true;
+	else
+		return false;
+}
+
+void ArrayList::addElement(int positon, ArrayListNode element)
+{
+	if (pElement != NULL)
+	{
+		if (positon >= 0 && positon < maxElementCount)
+		{
+			if (!isFull())
+			{
+				pElement[positon] = element;
+				currentElementCount++;
+				std::cout << "원소 추가 성공." << std::endl;
+				return;
+			}
+		}
+	}
+	std::cout << "Error: 원소 추가 실패." << std::endl;
+}
+
+void ArrayList::removeElement(int positon)
+{
+	if (pElement != NULL)
+	{
+		if (currentElementCount > 0)
+		{
+			if (positon >= 0 && positon < currentElementCount)
+			{
+				for (int i = positon; i < currentElementCount; i++)
+				{
+					pElement[positon] = pElement[positon+1];
+				}
+
+				ArrayListNode Null;
+				Null.data = 0;
+
+				pElement[positon] = Null;
+				std::cout << "원소 제거 성공." << std::endl;
+				return;
+			}
+		}
+	}
+	std::cout << "Error: 원소 제거 실패." << std::endl;
+}
+
+void ArrayList::clearList()
+{
+	ArrayListNode Null;
+	Null.data = 0;
+
+	for (int i = 0; i < currentElementCount; i++)
+		pElement[i] = Null;
+}
+
+void ArrayList::displayList()
+{
+	for (int i = 0; i < currentElementCount; i++)
+		std::cout << i << ": " << pElement[i].data << std::endl;
+}
+
+int ArrayList::getListLength()
+{
+	return currentElementCount;
+}
+
+ArrayListNode ArrayList::getElement(int positon)
+{
+	if (pElement != NULL)
+	{
+		if (positon >= 0 && positon < maxElementCount)
+		{
+			return pElement[positon];
+		}
+	}
 }
