bb819dcd3298a1ddb0df6cbeec99cd661aa635e4
여름 <pjw7200@ajou.ac.kr>
2021-01-16 17:58:04 +0900

    ADT와 변수명, 함수명을 참고하여 설계한 원형연결리스트

---
+++ Data-Structure/CircularLinkedListTest.cpp
@@ -0,0 +1,24 @@
+#include "CircularLinkedList.h"
+#include <iostream>
+
+int main(void)
+{
+	CircularLinkedList simpleLinkedList = CircularLinkedList();
+	ListNode node;
+	node.data = 0;
+	node.pLink = nullptr;
+
+	for (int i = 0; i < 5; i++)
+	{
+		simpleLinkedList.addElement(i, node);
+		node.data++;
+	}
+
+	simpleLinkedList.removeElement(0);
+	node.data = 10;
+	simpleLinkedList.addElement(3, node);
+	simpleLinkedList.displayList();
+
+	std::cout << simpleLinkedList.getElement(3)->data << std::endl;
+	std::cout << "Length: " << simpleLinkedList.getListLength() << std::endl;
+}
